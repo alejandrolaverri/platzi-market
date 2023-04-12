cafba2af0adc555f9ef53c2271d5adb409237b6f
Alejandro Moreno Olaverri <alejandro.moreno.olaverri@gmail.com>
2023-04-12 12:29:16 +0200

    Implementación de Query Methods

---
+++ src/main/java/com/alejandromo/platzimarket/persistence/ProductoRepository.java
@@ -0,0 +1,24 @@
+package com.alejandromo.platzimarket.persistence;
+
+import com.alejandromo.platzimarket.persistence.crud.ProductoCrudRepository;
+import com.alejandromo.platzimarket.persistence.entity.Producto;
+
+import java.util.List;
+import java.util.Optional;
+
+public class ProductoRepository {
+
+    private ProductoCrudRepository productoCrudRepository;
+
+    public List<Producto> getAll() {
+        return (List<Producto>) productoCrudRepository.findAll();
+    }
+
+    public List<Producto> getByCategoria(int idCategoria) {
+        return productoCrudRepository.findByIdCategoriaOrderByNombreAsc(idCategoria);
+    }
+
+    public Optional<List<Producto>> getEscasos(int cantidad) {
+        return productoCrudRepository.findByCantidadStockLessThanAndEstado(cantidad, true);
+    }
+}
---
+++ src/main/java/com/alejandromo/platzimarket/persistence/crud/ProductoCrudRepository.java
@@ -0,0 +1,13 @@
+package com.alejandromo.platzimarket.persistence.crud;
+
+import com.alejandromo.platzimarket.persistence.entity.Producto;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface ProductoCrudRepository extends CrudRepository<Producto, Integer> {
+    List<Producto> findByIdCategoriaOrderByNombreAsc(int idCategoria);
+    Optional<List<Producto>> findByCantidadStockLessThanAndEstado(int cantidadStock, boolean estado);
+
+}
