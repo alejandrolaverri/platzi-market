d1d0e0a2e672e4c13a176d88b9e5cae52402763c
Alejandro Moreno Olaverri <alejandro.moreno.olaverri@gmail.com>
2023-04-14 18:07:32 +0200

    Implementación del controlador ProductController y correción de errores

--- src/main/java/com/alejandromo/platzimarket/persistence/entity/ComprasProducto.java
+++ src/main/java/com/alejandromo/platzimarket/persistence/entity/ComprasProducto.java
@@ -16,9 +16,9 @@ public class ComprasProducto {
     private Boolean estado;
 
     @ManyToOne
+    @MapsId("idCompra")
     @JoinColumn(name = "id_compra", insertable = false, updatable = false)
     private Compra compra;
-
 
     @ManyToOne
     @JoinColumn(name = "id_producto", insertable = false, updatable = false)
@@ -54,5 +54,21 @@ public class ComprasProducto {
 
     public void setEstado(Boolean estado) {
         this.estado = estado;
+    }
+
+    public Compra getCompra() {
+        return compra;
+    }
+
+    public void setCompra(Compra compra) {
+        this.compra = compra;
+    }
+
+    public Producto getProducto() {
+        return producto;
+    }
+
+    public void setProducto(Producto producto) {
+        this.producto = producto;
     }
 }
