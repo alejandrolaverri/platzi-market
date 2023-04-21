d66c9524d1b445e5a93f6b108444f05737593869
Alejandro Moreno Olaverri <alejandro.moreno.olaverri@gmail.com>
2023-04-21 18:12:05 +0200

    Documentar API con SpringDoc y Swagger

--- src/main/java/com/alejandromo/platzimarket/web/controller/ProductController.java
+++ src/main/java/com/alejandromo/platzimarket/web/controller/ProductController.java
@@ -2,6 +2,10 @@ package com.alejandromo.platzimarket.web.controller;
 
 import com.alejandromo.platzimarket.domain.Product;
 import com.alejandromo.platzimarket.domain.service.ProductService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -16,12 +20,20 @@ public class ProductController {
     private ProductService productService;
 
     @GetMapping()
+    @Operation(description = "Get all supermarket products")
+    @ApiResponse(responseCode = "200", description = "OK")
     public ResponseEntity<List<Product>> getAll() {
         return new ResponseEntity<>(productService.getAll(), HttpStatus.OK);
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<Product> getProduct(@PathVariable("id") int productId) {
+    @Operation(description = "Search a product with an ID")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "OK"),
+            @ApiResponse(responseCode = "404", description = "Product not found")
+    })
+    public ResponseEntity<Product> getProduct(@Parameter(description = "The id of the product",required = true, example = "7")
+                                                  @PathVariable("id") int productId) {
         return ResponseEntity.of(productService.getProduct(productId));
     }
 
